d9b65d2722351a9495fde0fdac5cc2862aec9be1
Dadan Juanda <kang.dj749@gmail.com>
2026-01-05 07:49:28 +0700

    change midtrans route

--- src/app/api/midtrans/token/route.ts
+++ src/app/api/midtrans/token/route.ts
@@ -21,7 +21,6 @@ export async function POST(req: Request) {
     const snap = new midtransClient.Snap({
       isProduction: process.env.MIDTRANS_IS_PRODUCTION === "true",
       serverKey: process.env.MIDTRANS_SERVER_KEY!,
-      clientKey: process.env.MIDTRANS_CLIENT_KEY!,
     });
 
     /* ================= ITEMS ================= */
